be88e77c5ac94d8baf9bb27cff18de0a23b9ce4f
Eric Hurst <ericahurst77@gmail.com>
2022-04-24 16:52:30 -0400

    moved all files to new repo

---
+++ Develop/server.js
@@ -0,0 +1,57 @@
+// import express
+const express = require('express');
+//the following to bypass relative pathing
+const path = require('path');
+
+const index = require('../routes/index');
+
+//connecting the files 
+const dbData = require('./db.json');
+// const uuid = require('./helpers/uuid');
+
+const PORT = 3001;
+app.use('/api',index)
+const app = express();
+// middleware for parsing application.json and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+// GET request for a review
+
+api.get('/api/notes',(req,res) => 
+res.json(dbData) )
+
+// app.get('/api/notes', (req, res) => {
+//     const readFile= fs.readFileSync('./db.json', 'utf8');
+//     const notes = JSON.parse(readFile);
+//     res.json(notes)}
+// );
+
+// app.get('/',(req,res) => res.sendFile(path.join(__dirname,'public/index.html')))
+
+//Get request
+app.get('/notes', (req, res) => 
+ res.sendFile(path.join(__dirname, '/public/notes.html' ))
+);
+
+app.get('/', (req, res) => 
+ res.sendFile(path.join(__dirname, '/public/index.html'))
+ );
+
+ app.get('*', (req, res) =>
+ res.sendFile(path.join(__dirname, '/public/index.html'))
+ );
+
+ 
+
+    
+    
+
+
+ app.listen(PORT, () => 
+console.log(`Sample text listening at http://localhost:${PORT}`))
+
+ 
+
+
